d1d8f8bb341f077d1a102a4a5c735169d4be3771
Asfia Khatoon <hafsasmum@gmail.com>
2021-11-15 22:13:05 +0800

    updated sql file

--- createtables.sql
+++ createtables.sql
@@ -11,29 +11,28 @@ SELECT * FROM departments;
 
 CREATE TABLE dept_emp (
     emp_no INT   NOT NULL,
-    dept_no VARCHAR   NOT NULL,
-    from_date DATE   NOT NULL,
-    to_date DATE   NOT NULL
+    dept_no VARCHAR   NOT NULL
+    
 );
 
 SELECT * FROM dept_emp;
 
 CREATE TABLE dept_manager (
     dept_no VARCHAR   NOT NULL,
-    emp_no INT   NOT NULL,
-    from_date DATE   NOT NULL,
-    to_date DATE   NOT NULL
+    emp_no INT   NOT NULL
+    
 );
 
 SELECT * FROM dept_manager;
 
 CREATE TABLE employees (
     emp_no INT   NOT NULL,
-    birth_date DATE   NOT NULL,
+    emp_title_id INT 
+    birth_date VARCHAR   NOT NULL,
     first_name VARCHAR   NOT NULL,
     last_name VARCHAR   NOT NULL,
     gender VARCHAR   NOT NULL,
-    hire_date DATE   NOT NULL,
+    hire_date VARCHAR   NOT NULL,
     CONSTRAINT pk_employees PRIMARY KEY (
        emp_no
      )
@@ -43,17 +42,15 @@ SELECT * FROM employees;
 
 CREATE TABLE salaries (
     emp_no INT   NOT NULL,
-    salary INT   NOT NULL,
-    from_date DATE   NOT NULL,
-    to_date DATE   NOT NULL
+    salary INT   NOT NULL
+    
 );
 SELECT * FROM salaries;
 
 CREATE TABLE titles (
-    emp_no INT   NOT NULL,
-    title VARCHAR   NOT NULL,
-    from_date DATE   NOT NULL,
-    to_date DATE   NOT NULL
+    title_id INT   NOT NULL,
+    title VARCHAR   NOT NULL
+    
 );
 SELECT * FROM titles;
 
@@ -72,7 +69,8 @@ REFERENCES "employees" ("emp_no");
 ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
 REFERENCES "employees" ("emp_no");
 
-ALTER TABLE titles ADD CONSTRAINT fk_titles_emp_no FOREIGN KEY(emp_no)
+ALTER TABLE titles ADD CONSTRAINT fk_titles_emp_title_id FOREIGN KEY(title_id)
 REFERENCES employees (emp_no);
+
 
 
